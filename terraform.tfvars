7e41e09d7487b839f7792e6c174a16ecca2f17a6
Nithesh Balakrishnan <antechnologiesindia@gmail.com>
2024-08-09 21:10:10 +0530

    Move variable values to terraform.tfvars

---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+region = "us-west-2"
+vpc_cidr = "10.0.0.0/16"
+availability_zones = ["us-west-2a", "us-west-2b"]
+cluster_name = "main-eks-cluster"
--- variables.tf
+++ variables.tf
@@ -1,23 +1,19 @@
 variable "region" {
   description = "The AWS region to create resources in"
   type        = string
-  default     = "us-west-2"
 }
 
 variable "vpc_cidr" {
   description = "The CIDR block for the VPC"
   type        = string
-  default     = "10.0.0.0/16"
 }
 
 variable "availability_zones" {
   description = "List of availability zones to use for the subnets"
   type        = list(string)
-  default     = ["us-west-2a", "us-west-2b"]
 }
 
 variable "cluster_name" {
   description = "The name of the EKS cluster"
   type        = string
-  default     = "main-eks-cluster"
 }
